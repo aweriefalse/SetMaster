5abb736b8af0d195543d2aac90a40a231a3416e8
aweriefalse <space.up.russia@gmail.com>
2025-04-28 00:27:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,104 @@
 import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 import constants as const
 import windows as ws
 
-
-def focus(event):
-    if event:
-        main_window.config(takefocus=True)
 
 def start_main_program():
     global main_window
     main_window = MainWindow()
     main_window.focus_force()
 
+    def chapter1_start():
+        main_window.destroy()
+        ws.chapter1_start()
+
+    style = ttk.Style()
+    style.configure(
+        "My.TFrame",
+        background=const.BACKGROUND_COLOR,
+        borderwidth=const.BORDER_WIDTH
+    )
+
+    text_style = ttk.Style()
+    text_style.configure(
+        "My.TLabel",
+        background=const.BACKGROUND_COLOR,
+        foreground="white",
+        font=(const.DEFAULT_FONT, 15, "bold")
+    )
+
+    tabline = ttk.Notebook(main_window, height = 20, style="My.TLabel")
+
+    main_menu = ttk.Frame(tabline, style="My.TFrame")
+    theory_practice = ttk.Frame(tabline, style="My.TFrame")
+    set_calc = ttk.Frame(tabline, style="My.TFrame")
+
+    tabline.add(main_menu, text="Главное меню")
+    tabline.add(theory_practice, text="Теория & Практика")
+    tabline.add(set_calc, text="Калькулятор множеств")
+    tabline.pack(expand=True, fill="both")
+
+    info_label = tk.Label(
+        main_menu,
+        text=const.WELCOME.format(const.USERNAME),
+        font=(const.DEFAULT_FONT2, 16, "bold"),
+        bg=const.BACKGROUND_COLOR,
+        fg="white",
+        wraplength=const.MWINDOW_WIDTH - 40
+    )
+    info_label.pack(pady=10, anchor="n")
+
+    made_by_label = tk.Label(
+        main_menu,
+        text=const.MADE_BY,
+        font=(const.DEFAULT_FONT2, 14),
+        bg=const.BACKGROUND_COLOR,
+        fg="lightgrey"
+    )
+    made_by_label.place(relx=1, rely=1, anchor="se")
+
+    intersection_img = Image.open("Images/intersection1.png")
+    intersection_img = ImageTk.PhotoImage(intersection_img.resize((200, 132)))
+    intersection_label = tk.Label(
+        main_menu,
+        image=intersection_img,
+        bg=const.BACKGROUND_COLOR
+    )
+    intersection_label.image = intersection_img
+    intersection_label.place(relx=0.5, rely=0.62, anchor="center")
+
+
+    chapter1 = tk.Button(
+        theory_practice,
+        text="Глава 1.\nПонятие множества и базовые примеры.",
+        font=(const.DEFAULT_FONT, 16),
+        height = theory_practice.winfo_height() // 3,
+        width = theory_practice.winfo_width() // 5,
+        borderwidth=const.BORDER_WIDTH,
+        bg="grey",
+        fg="white",
+        command=chapter1_start
+    )
+    chapter1.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.2)
+
+    chapter2 = tk.Button(
+        theory_practice,
+        text="Глава 2.\nОперации со множествами. Примеры задач.",
+        font=(const.DEFAULT_FONT, 16),
+        height=theory_practice.winfo_height() // 3,
+        width=theory_practice.winfo_width() // 5,
+        borderwidth=const.BORDER_WIDTH,
+        bg="grey",
+        fg="white",
+       # command=ws.chapter2_start
+    )
+    chapter2.place(relx=0.05, rely=0.3, relwidth=0.9, relheight=0.2)
+
     main_window.bind("<F11>", main_window.max_minimize)
     main_window.bind("<Escape>", main_window.kill)
 
-    main_window.after(250, focus, 1)
     main_window.mainloop()
 
 class MainWindow(tk.Tk):
@@ -36,7 +119,9 @@ class MainWindow(tk.Tk):
             borderwidth=const.BORDER_WIDTH,
             takefocus=False
         )
+        self.minsize(width=const.MWINDOW_WIDTH, height=const.MWINDOW_HEIGHT)
 
+        self.iconbitmap("Images/dialog_logo.ico")
         self.title("Set Master")
 
     def max_minimize(self, event):
--- windows.py
+++ windows.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+import customtkinter as ctk
 from tkinter.messagebox import showinfo
+from PIL import Image, ImageTk
+import webbrowser
 import constants as const
 import main
 
@@ -27,15 +30,25 @@ class WelcomeWindow(tk.Tk):
 
         def throw_username(event=None):
             if len(username_inp.get()) >= 3:
-                if len(username_inp.get()) > 12:
-                    username = f"{username_inp.get()[:12]}..."
+                if len(username_inp.get()) > 15:
+                    username = f"{username_inp.get()[:15]}..."
                 else:
                     username = username_inp.get()
                 const.USERNAME = username
+                print(f"USERNAME: {const.USERNAME}")
                 self.destroy()
                 main.start_main_program()
             else:
                 showinfo("Некорректные данные", "Минимальная длина имени:\n3 символа")
+
+        def open_page(event=None):
+            self.withdraw()
+            try:
+                webbrowser.open("https://estaldialog.edumsko.ru/")
+                self.attributes("-topmost", 1)
+                self.after(1000, lambda: self.attributes("-topmost", 0))
+            except Exception as e:
+                print(f"Request has not been completed due to error:\n{e}")
 
         def kill(event=None):
             self.destroy()
@@ -49,12 +62,17 @@ class WelcomeWindow(tk.Tk):
         )
         welcome_label.pack(pady=10)
 
-        dialog_logo = tk.PhotoImage(file="Images/dialog_logo.png", width=50, height=50, format="png")
-        welcome_logo = tk.Label(
+        img = Image.open("Images/dialog_logo.png")
+        dialog_logo = ImageTk.PhotoImage(img.resize((50, 50)))
+        welcome_logo_button = tk.Button(
             self,
-            image=dialog_logo
+            image=dialog_logo,
+            bg=const.BACKGROUND_COLOR,
+            borderwidth=const.BORDER_WIDTH,
+            command=open_page
         )
-        welcome_logo.pack(pady=5)
+        welcome_logo_button.image = dialog_logo
+        welcome_logo_button.pack()
 
         ask_name_label = tk.Label(
             self,
@@ -62,16 +80,17 @@ class WelcomeWindow(tk.Tk):
             font=(const.DEFAULT_FONT, 16, "bold"),
             fg="lightgrey", bg=const.BACKGROUND_COLOR
         )
-        ask_name_label.place(relx=0.05, rely=0.5)
+        ask_name_label.place(relx=0.05, rely=0.6)
 
         username_inp = tk.Entry(
             self,
             width=14,
             bg="lightgrey",
             fg=const.BACKGROUND_COLOR,
-            font=(const.DEFAULT_FONT2, 15, "bold")
+            font=(const.DEFAULT_FONT2, 15, "bold"),
+            validate="focus"
         )
-        username_inp.place(relx=0.05, rely=0.65)
+        username_inp.place(relx=0.05, rely=0.75)
 
         throw_username_button = tk.Button(
             self,
@@ -83,12 +102,66 @@ class WelcomeWindow(tk.Tk):
             bg=const.BACKGROUND_COLOR,
             command=throw_username
         )
-        throw_username_button.place(relx=0.7, rely=0.615)
 
+        throw_username_button.place(relx=0.7, rely=0.715)
         self.bind("<Return>", throw_username)
+        self.bind("<Escape>", kill)
+
+
+class ReadWindow(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        def fullscreen(event):
+            if event:
+                if self.attributes("-fullscreen"):
+                    self.attributes("-fullscreen", False)
+                else:
+                    self.attributes("-fullscreen", True)
+
+        def kill(event):
+            self.destroy()
+            main.start_main_program()
+
+        self.iconbitmap("Images/dialog_logo.ico")
+        self.title("Set Master")
+
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        self.resizable(True, True)
+        size = f"{const.MWINDOW_WIDTH}x{const.MWINDOW_HEIGHT}"
+        pos = f"+{screen_width // 2 - const.MWINDOW_WIDTH // 2}+{screen_height // 2 - const.MWINDOW_HEIGHT // 2}"
+        self.geometry(f"{size}{pos}")
+        self.minsize(width=const.MWINDOW_WIDTH, height=const.MWINDOW_HEIGHT)
+
+        self.config(
+            bg=const.BACKGROUND_COLOR2
+        )
+
+        self.bind("<F11>", fullscreen)
         self.bind("<Escape>", kill)
 
 
 def open_welcome_window():
     welcome_window = WelcomeWindow()
     welcome_window.mainloop()
+
+def chapter1_start():
+    chapt1_window = ReadWindow()
+
+    text1 = ctk.CTkTextbox(
+        chapt1_window,
+        text_color="white",
+        font=(const.DEFAULT_FONT2, 22),
+        corner_radius=20,
+        wrap="word"
+    )
+    text1.insert("0.0", const.INFO1)
+    text1.configure(state="disabled")
+    text1.place(relx=0.99, rely=0.01, relwidth=0.78, relheight=0.98, anchor="ne")
+
+    chapt1_window.mainloop()
